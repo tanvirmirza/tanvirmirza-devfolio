ddbd77b6601a8c51ef157ec752e284b6d4d24746
Mirza Tanvir <97517155+tanvirmirza@users.noreply.github.com>
2025-08-03 18:57:47 +0600

    build project structure

--- android/app/src/main/kotlin/com/example/mirza_tanvir_devfolio/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mirza_tanvir_devfolio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ tanvirmirza_dev/android/app/src/main/kotlin/com/example/tanvirmirza_dev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tanvirmirza_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
